2ab80a10ea62db8b41406ecc310bbf173561804d
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2018-11-18 16:22:50 +0000

    fetch_exercise1.8

---
+++ chapter_01/fetch_exercise1.8.go
@@ -0,0 +1,34 @@
+// Fetch prints the content found at a URL.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	var processed_url string
+
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			processed_url = "http://" + url
+		} else {
+			processed_url = url
+		}
+		resp, err := http.Get(processed_url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
