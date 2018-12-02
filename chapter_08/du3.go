0100a5cec3c53dee29320f67e24533af9bdc03e8
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2018-12-02 18:03:41 +0000

    du

--- chapter_08/du3.go
+++ chapter_08/du3.go
@@ -1,7 +1,8 @@
-package main
+// The du3 variant traverses all directories in parallel.
+// It uses a concurrency-limiting counting semaphore
+// to avoid opening too many files at once.
 
-// The du2 variant uses select and a time.Ticker
-// to print the totals periodically if -v is set.
+package main
 
 import (
 	"flag"
@@ -9,35 +10,37 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 )
 
-var verbose = flag.Bool("v", false, "show verbose progress messages")
+var vFlag = flag.Bool("v", false, "show verbose progress messages")
 
 func main() {
-	// ...start background goroutine...
-
-	//!-
-	// Determine the initial directories.
 	flag.Parse()
+
+	// Determine the initial directories.
 	roots := flag.Args()
 	if len(roots) == 0 {
 		roots = []string{"."}
 	}
 
-	// Traverse the file tree.
+	// Traverse each root of the file tree in parallel.
 	fileSizes := make(chan int64)
+	var n sync.WaitGroup
+	for _, root := range roots {
+		n.Add(1)
+		go walkDir(root, &n, fileSizes)
+	}
 	go func() {
-		for _, root := range roots {
-			walkDir(root, fileSizes)
-		}
+		n.Wait()
 		close(fileSizes)
 	}()
 
 	// Print the results periodically.
 	var tick <-chan time.Time
-	if *verbose {
-		tick = time.Tick(100 * time.Millisecond)
+	if *vFlag {
+		tick = time.Tick(500 * time.Millisecond)
 	}
 	var nfiles, nbytes int64
 loop:
@@ -53,6 +56,7 @@ loop:
 			printDiskUsage(nfiles, nbytes)
 		}
 	}
+
 	printDiskUsage(nfiles, nbytes) // final totals
 }
 
@@ -60,20 +64,25 @@ func printDiskUsage(nfiles, nbytes int64) {
 	fmt.Printf("%d files  %.1f GB\n", nfiles, float64(nbytes)/1e9)
 }
 
-// walkDir recursively walks the file tree rooted at dir
-// and sends the size of each found file on fileSizes.
-func walkDir(dir string, fileSizes chan<- int64) {
+func walkDir(dir string, n *sync.WaitGroup, fileSizes chan<- int64) {
+	defer n.Done()
 	for _, entry := range dirents(dir) {
 		if entry.IsDir() {
+			n.Add(1)
 			subdir := filepath.Join(dir, entry.Name())
-			walkDir(subdir, fileSizes)
+			go walkDir(subdir, n, fileSizes)
 		} else {
 			fileSizes <- entry.Size()
 		}
 	}
 }
 
+var sema = make(chan struct{}, 20)
+
 func dirents(dir string) []os.FileInfo {
+	sema <- struct{}{}        // acquire token
+	defer func() { <-sema }() // release token
+
 	entries, err := ioutil.ReadDir(dir)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "du: %v\n", err)
